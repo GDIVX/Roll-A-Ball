245c2ff8b144ca5ec586d6c41c98f8d8a8fb8254
GDIVX <yarin107@gmail.com>
2022-09-29 12:24:08 +0300

    Add project files.

---
+++ Assets/_Completed-Game/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour {
+
+    public Scene gameScene;
+
+    public void StartGameBtn(){
+        
+    }
+
+    public void QuitAppliationBtn(){
+
+    }
+}
